366fbc8f2b6b0cb23f1c0f52d0d5320febb78fb7
classicsman <46722413+classicsman@users.noreply.github.com>
2020-07-28 12:18:05 -0400

    Added main part of program

---
+++ fullLibraryListFormatter.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+# bringing the library items together in one place
+from libraryListFormatter2LFPL import outputLFPL, numOfItemsLFPL
+from libraryListFormatter2NAFC import outputNAFC, numOfItemsNAFC
+from libraryListFormatter2CCCO import outputCCCO, numOfItemsCCCO
+
+now = datetime.now()
+
+# Date format for file name
+formattedDate = now.strftime('%Y-%m-%d')
+
+# Countdown til we get out of this awful year
+newYear = datetime.strptime('Jan 1 2021  00:00', '%b %d %Y %H:%M')
+
+countdown = int((newYear-now).total_seconds())
+
+days = countdown // 86400
+hours = (countdown - days * 86400) // 3600
+minutes = (countdown - days * 86400 - hours * 3600) // 60
+seconds = countdown - days * 86400 - hours * 3600 - minutes * 60
+betterTimes = 'Only {} days, {} hours, {} minutes, and {} seconds left until we get a (HOPEFULLY) \
+better year in 2021!\n\n'.format(days, hours, minutes, seconds)
+
+# counts up all the items out
+totalItems = numOfItemsLFPL + numOfItemsNAFC + numOfItemsCCCO
+
+# concatenates Countdown and reformatted text files
+outputFile = open(f'library_books_out_{formattedDate}.txt', "w")
+outputFile.writelines(betterTimes + outputLFPL + outputNAFC + outputCCCO + \
+                      str("\n\n\nTotal number of items checked out: {}".format(totalItems)))
+outputFile.close()
+
